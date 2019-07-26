5f0464c23113b8a589e3afb0d4213e7b1f17fc1d
dohertre <dohertre@rose-hulman.edu>
2019-07-25 21:39:33 -0400

    random

--- src/m6_loops_within_loops_printing.py
+++ src/m6_loops_within_loops_printing.py
@@ -75,7 +75,7 @@ def shape(r):
     For purposes of "lining up", assume r is a single digit.
     """
     # ------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #          Some tests are already written for you (above).
     #
     ####################################################################
